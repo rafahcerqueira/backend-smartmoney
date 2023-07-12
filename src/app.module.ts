120fadb2e7c69bcaa1a4892760e9b2f22c33fb04
Rafael <rafahcerqueira.123@gmail.com>
2023-07-12 19:05:17 -0300

    add: criando backend smartmoney ja com autenticacao e crud de usuario pronto

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+
+@Module({
+  imports: [PrismaModule, UserModule, AuthModule],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+})
+export class AppModule {}
